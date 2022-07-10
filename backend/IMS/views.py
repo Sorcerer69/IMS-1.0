1867f5060e0cc552944675f9b0f59247a21168c7
Sorcerer69 <82149491+Sorcerer69@users.noreply.github.com>
2022-07-10 18:28:34 +0545

    Template for products

--- backend/IMS/views.py
+++ backend/IMS/views.py
@@ -1,16 +1,21 @@
 
 from urllib import request
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth. models import User, auth
 from django.contrib import messages
+from django.views import View
+
+import IMS
 
 # Create your views here.
+
+
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        user= auth.authenticate(username=username, password=password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
@@ -22,8 +27,14 @@ def login(request):
     else:
         return render(request, 'login.html')
 
+
 def dashboard(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'title': 'Dashboard - IMS'})
+
+
+def products(request):
+    return render(request, '../products.html', {'title': 'Products - IMS'})
+
 
 def logout(request):
     auth.logout(request)
